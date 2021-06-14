a4e1dc8c56bf5ebdf74c762fd1e9d2732562f7a8
annakhuseinova <annakhuseinova@yandex.ru>
2021-06-14 21:37:58 +0300

    Initial commit

---
+++ src/main/java/com/annakhuseinova/springcloudstreamswindowingaggregates/SpringCloudStreamsWindowingAggregatesApplication.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springcloudstreamswindowingaggregates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamsWindowingAggregatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamsWindowingAggregatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/annakhuseinova/springcloudstreamswindowingaggregates/SpringCloudStreamsWindowingAggregatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springcloudstreamswindowingaggregates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamsWindowingAggregatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
